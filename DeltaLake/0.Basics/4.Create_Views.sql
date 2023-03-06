923a325860bba3d04707a8b482dfbcf13353c004
depariksheet2@gmail.com <depariksheet2@gmail.com>
2023-03-06 14:46:15 +0000

    commit

---
+++ DeltaLake/0.Basics/4.Create_Views.sql
@@ -0,0 +1,71 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC ##### CREATE A PERSISTANT VIEW
+
+-- COMMAND ----------
+
+CREATE VIEW IF NOT EXISTS delta.emp_wise_loc_dept_vw
+AS
+SELECT 
+l.loc_id,
+l.loc_name,
+d.dept_name
+FROM departments d JOIN delta.locations l
+ON d.loc_id = l.loc_id;
+
+-- COMMAND ----------
+
+SELECT * FROM delta.emp_wise_loc_dept_vw;
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ##### CREATE A TEMPORARY VIEW
+
+-- COMMAND ----------
+
+CREATE TEMP VIEW IF NOT EXISTS emp_wise_loc_dept_temp_vw
+AS
+SELECT 
+l.loc_id,
+l.loc_name,
+d.dept_name
+FROM departments d JOIN delta.locations l
+ON d.loc_id = l.loc_id;
+
+-- COMMAND ----------
+
+SELECT * FROM emp_wise_loc_dept_temp_vw;
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ##### CREATE A GLOBAL TEMPORARY VIEW
+
+-- COMMAND ----------
+
+CREATE GLOBAL TEMP VIEW emp_wise_loc_dept_global_temp_vw
+AS
+SELECT 
+l.loc_id,
+l.loc_name,
+d.dept_name
+FROM departments d JOIN delta.locations l
+ON d.loc_id = l.loc_id;
+
+-- COMMAND ----------
+
+SELECT * FROM emp_wise_loc_dept_global_temp_vw;
+
+-- COMMAND ----------
+
+SHOW TABLES
+
+-- COMMAND ----------
+
+SHOW TABLES IN global_temp;
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC dbutils.notebook.exit("EXECUTED SUCCESSFULLY")
