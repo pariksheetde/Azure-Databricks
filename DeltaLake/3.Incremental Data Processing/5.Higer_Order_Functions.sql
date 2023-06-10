f25631568e31900e3f8bf85049bade32d55dcf09
depariksheet2@gmail.com <depariksheet2@gmail.com>
2023-06-10 04:52:07 +0000

    --COMMIT - Higher_Order_Function

--- DeltaLake/3.Incremental Data Processing/5.Higer_Order_Functions.sql
+++ DeltaLake/3.Incremental Data Processing/5.Higer_Order_Functions.sql
@@ -38,5 +38,53 @@ FROM dw_analytics.orders o;
 
 -- COMMAND ----------
 
+CREATE OR REPLACE FUNCTION get_url (email STRING)
+RETURNS STRING
+
+RETURN concat("https://www.", split(email, "@")[1])
+
+-- COMMAND ----------
+
+CREATE OR REPLACE FUNCTION site_type (email STRING)
+RETURNS STRING
+
+RETURN CASE 
+      WHEN email like "%.com" THEN "Commercial Business"
+      WHEN email like "%.org" THEN "Non-Proft Business"
+      WHEN email like "%.edu" THEN "Educational Institution"
+      ELSE concat("Unknown extension for domain: ", split(email, "@")[1])
+    END;
+
+-- COMMAND ----------
+
+CREATE OR REPLACE FUNCTION get_domain (email STRING)
+RETURNS STRING
+
+RETURN (split(email, "@")[1])
+
+-- COMMAND ----------
+
+SELECT 
+email
+,get_url(email) as url
+,get_domain(email) as domain
+,site_type(email) as category
+FROM
+dw_analytics.customers;
+
+-- COMMAND ----------
+
+CREATE OR REPLACE FUNCTION site_type (email STRING)
+RETURNS STRING
+
+RETURN CASE 
+      WHEN email like "%.com" THEN "Commercial Business"
+      WHEN email like "%.org" THEN "Non-Proft Business"
+      WHEN email like "%.edu" THEN "Educational Institution"
+      ELSE concat("Unknown extension for domain", split(email, "@")[1])
+    END;
+
+-- COMMAND ----------
+
 -- MAGIC %python
 -- MAGIC dbutils.notebook.exit("EXECUTED SUCCESSFULLY")
