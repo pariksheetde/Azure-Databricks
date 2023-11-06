9760824727d48e4bb2b7dbaa031773626398781a
depariksheet2@gmail.com <depariksheet2@gmail.com>
2023-11-06 14:51:21 +0000

    COMMIT

---
+++ DeltaLake/5.Multi-Hop Lambda Architecture/1.Bronze/2.validate.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+files = dbutils.fs.ls("/mnt/adobeadls/dwanalytics/orders/orders-raw/")
+display(files)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT COUNT(*) AS cnt FROM bronze.orders;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM silver.orders;
